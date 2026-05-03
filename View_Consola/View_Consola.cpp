935a3acb5b20679f2067362780bc1fa83ffe1b37
Danny <dan051020@gmail.com>
2026-05-03 10:51:22 -0500

    cambios

--- View_Consola/View_Consola.cpp
+++ View_Consola/View_Consola.cpp
@@ -5,5 +5,7 @@ using namespace System;
 int main(array<System::String ^> ^args)
 {
     //Estos son mis cambios Danny
+
+    //Sigo esperando que alguno de mis chicos me pongan su nombre de usuario
     return 0;
 }
