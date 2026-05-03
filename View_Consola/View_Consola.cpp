6a93b92c0ef5cb20593425546ae871389247fc1a
chamb <a20222215@pucp.edu.pe>
2026-05-03 11:10:46 -0500

    ok

--- View_Consola/View_Consola.cpp
+++ View_Consola/View_Consola.cpp
@@ -8,7 +8,7 @@ int main(array<System::String ^> ^args)
 
     //Sigo esperando que alguno de mis chicos me pongan su nombre de usuario
 
-    //Ya lo cloné
+    //Ya lo cloné :v
     //Estos cambios son hechos desde la nube de github
     return 0;
 }
